4543d600f0418d7d0c727bca0c812c9063fb27be
Abrahan Romero <aromero@lendingclub.com>
2018-12-12 14:51:16 -0800

    Initial commit

---
+++ src/main/java/com/aromero/webflux/aromerowebflux/AromeroWebfluxApplication.java
@@ -0,0 +1,14 @@
+package com.aromero.webflux.aromerowebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AromeroWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AromeroWebfluxApplication.class, args);
+	}
+
+}
+
